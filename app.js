b990b3841a64d40703a846f24155eb81a55038ba
eduardohdzr <eduardo_hdzr@live.com.mx>
2025-01-24 12:06:42 -0600

    Cargar version 1 del juego

--- app.js
+++ app.js
@@ -1,1 +1,36 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+// El principal objetivo de este desafío es
+// fortalecer tus habilidades en lógica de programación.
+// Aquí deberás desarrollar la lógica para resolver el problema.
+
+let amigos = [];
+
+function agregarAmigo() {
+    let valorDeUsuario = document.getElementById('valorUsuario').value;
+    if (valorDeUsuario == '') {
+       alert('Por favor, inserte un nombre');
+    } else {
+        document.getElementById('valorUsuario').value = '';
+        amigos.push(valorDeUsuario);
+        listaAmigos();
+    }
+}
+
+function listaAmigos() {
+    let lista = document.getElementById('listaAmigos');
+    let nuevaLista = '';
+    for(let i = 0; i <= amigos.length - 1; i++){
+        nuevaLista = '<li>' + amigos[i] + '</li>';
+    }
+    return lista.innerHTML += nuevaLista;
+}
+function sortearAmigo() {
+    let indiceAleatorio = 0;
+    if (amigos == ''){
+        alert('Inserte un nombre');
+    } else {
+        document.getElementById('listaAmigos').innerHTML = "";
+        indiceAleatorio = Math.floor(Math.random()* amigos.length);
+        let resultado = document.getElementById('resultado');
+        resultado.innerHTML = `El amigo secreto es ${amigos[indiceAleatorio]}`;
+    } return;
+}
